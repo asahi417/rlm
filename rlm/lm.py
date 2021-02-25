0c2db793595b6ca4d8bc8475e2b5dc14d1c0114c
asahi417 <asahi1992ushio@gmail.com>
2021-02-25 17:19:57 +0000

    init

--- rlm/lm.py
+++ rlm/lm.py
@@ -71,10 +71,9 @@ class RelationEmbedding:
         self.cache_dir = cache_dir
         self.device = None
         self.model = None
+        self.tokenizer = None
+        self.config = None
         self.max_length = max_length
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model, cache_dir=cache_dir)
-        self.config = transformers.AutoConfig.from_pretrained(
-            model, cache_dir=cache_dir, output_attentions=True, output_hidden_states=True)
         self.num_hidden_layers = self.config.num_hidden_layers
 
     def __load_model(self):
@@ -82,6 +81,9 @@ class RelationEmbedding:
         if self.model:
             return
         logging.debug('loading language model')
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_name, cache_dir=self.cache_dir)
+        self.config = transformers.AutoConfig.from_pretrained(
+            self.model_name, cache_dir=self.cache_dir, output_attentions=True, output_hidden_states=True)
         self.model = transformers.AutoModelForMaskedLM.from_pretrained(
             self.model_name, config=self.config, cache_dir=self.cache_dir)
         self.model.eval()
