c8ff52102072114664009840b8b74934bc4c4873
asahi417 <asahi1992ushio@gmail.com>
2021-02-25 00:23:38 +0000

    init

--- examples/main.py
+++ examples/main.py
@@ -1,4 +1,7 @@
+import logging
 import rlm
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main(lm):
@@ -7,10 +10,11 @@ def main(lm):
     scorer.analogy_test(test=False, data=data, batch_size=batch)
 
 
-data = 'sat'
-batch = 512
-length = 16
-models = ['roberta-large', 'bert-large-cased']
-for m in models:
-    main(m)
-rlm.export_csv_summary()
+if __name__ == '__main__':
+    data = 'sat'
+    batch = 512
+    length = 16
+    models = ['roberta-large', 'bert-large-cased']
+    for m in models:
+        main(m)
+    rlm.export_csv_summary()
