5c5e8a43c9fbda71bb28c5f08d1ae1a4ea5f332e
asahi417 <asahi1992ushio@gmail.com>
2021-02-26 11:12:57 +0000

    init

--- examples/main.py
+++ examples/main.py
@@ -2,17 +2,20 @@ import logging
 import rlm
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+batch = 512
+length = 16
+negative_permutation_weight = [0, 0.2, 0.4, 0.6]
 
 
 def main(lm):
     scorer = rlm.Scorer(lm, max_length=length)
-    scorer.analogy_test(test=True, data=data, batch_size=batch, method=['embedding_cos', 'embedding_euc'])
-    scorer.analogy_test(test=False, data=data, batch_size=batch, method=['embedding_cos', 'embedding_euc'])
+    scorer.analogy_test(test=True, data=data, batch_size=batch, method=['embedding_cos', 'embedding_euc'],
+                        negative_permutation_weight=negative_permutation_weight)
+    scorer.analogy_test(test=False, data=data, batch_size=batch, method=['embedding_cos', 'embedding_euc'],
+                        negative_permutation_weight=negative_permutation_weight)
 
 
 if __name__ == '__main__':
-    batch = 512
-    length = 16
     # bats is large as it gets to be ~ 50GB in total
     for data in ['sat', 'google', 'u2', 'u4']:
         for model in ['roberta-large', 'bert-large-cased']:
